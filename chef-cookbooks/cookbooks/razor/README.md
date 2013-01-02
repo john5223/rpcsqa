b8e33854d55fe1b13b8ade25c0d35fc0ace9cc40
solo4572 <jacob.wagner@rackspace.com>
2013-01-02 13:30:53 -0600

    added README and metdata.rb to razor cookbooks

---
+++ chef-cookbooks/cookbooks/razor/README.md
@@ -0,0 +1,11 @@
+Usage
+=====
+
+Create a data_bag in your cookbook repo that is id'd by your boxes ethernet mac addresses
+
+ex.
+
+{
+    "id": "D4AE527F19F6_D4AE527F19F8_D4AE527F19FA_D4AE527F19FC"
+    "name": "Test Server 1"
+}
---
+++ chef-cookbooks/cookbooks/razor/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Rackspace U.S. Inc."
+maintainer_email "jacob.wagner@rackspace.com"
+license          "All rights reserved"
+description      "Pulls data_bags for a environment provisioned by raozr"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
