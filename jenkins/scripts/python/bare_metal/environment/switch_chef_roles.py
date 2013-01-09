385a0f650afdf09a04fc2a37e26270c12cd17a42
solo4572 <jacob.wagner@rackspace.com>
2013-01-09 16:46:02 -0600

    testing the chef python api

--- jenkins/scripts/python/bare_metal/environment/switch_chef_roles.py
+++ jenkins/scripts/python/bare_metal/environment/switch_chef_roles.py
@@ -7,9 +7,6 @@ from ssh_session import ssh_session
 import time
 from chef import *
 
-import sys
-sys.stdout.flush()
-
 
 parser = argparse.ArgumentParser()
 # Get the ip of the server you want to remove
@@ -24,34 +21,24 @@ parser.add_argument('--data-bag-location', action="store", dest="data_bag_loc",
                     default="/var/lib/jenkins/rpcsqa/chef-cookbooks/data_bags/razor_node", 
                     required=False, help="Policy to teardown from razor and reboot nodes")
 
-
-
 parser.add_argument('--chef-url', action="store", dest="chef_url", 
                     default="http://198.101.133.4:4000", 
                     required=False, help="client for chef")
-
 
 parser.add_argument('--chef-client', action="store", dest="chef_client", 
                     default="jenkins", 
                     required=False, help="client for chef")
 
-
 parser.add_argument('--chef-client-pem', action="store", dest="chef_client_pem", 
                     default="/var/lib/jenkins/rpcsqa/.chef/jenkins.pem", 
                     required=False, help="client pem for chef")
-
-
-
 
 parser.add_argument('--display-only', action="store", dest="display_only", 
                     default="true", 
                     required=False, help="Display the node information only (will not reboot or teardown am)")
 
-
 # Parse the parameters
 results = parser.parse_args()
-
-
 
 def get_data_bag_UUID(data):
     try:
@@ -89,20 +76,14 @@ def getip_from_data_bag(uuid):
 #Collect active models that match policy from given input
 #############################################################
 
-#####
-
-
-
 razor = razor_api(results.razor_ip)
 policy = results.policy
-
 
 print "#################################"
 print " Switching roles and running chef-client for  '%s'  active models" % policy
 print "Display only: %s " % results.display_only
 
 active_models = razor.simple_active_models(policy)
-
 
 if active_models == {}:
     print "'%s' active models: 0 " % (policy)
@@ -136,12 +117,10 @@ else:
         #SSH into ip and reboot 
         print "Active Model ID: %s " % active
         print "Data Bag UUID: %s " % dbag_uuid
-        #print "ROOT_PASS: %s " % root_pass
         print "Public address: %s " % ip
         print "Private address: %s " % private_ip
         print ""
-    
-        
+
         if results.display_only == 'false':
             
             print "Trying to switch roles and run chef-client for %s...." % ip
@@ -155,29 +134,11 @@ else:
                 print "FAILURE: %s " % e
             finally:
                 session.close()
-                
-            
-    print hosts
  
     with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
-            for host in hosts:
-                node = Node(host)
-                print node.run_list
-                print node.chef_environment
-                
-                #Change environment
-                #node.chef_environment = 'rpcs'
-                #Change run list
-                node.run_list = ['role[controller]']
-                
-                node.save
-                
-                node = Node(host)
-                print node.run_list
-                print node.chef_environment
-                
-                break
+        for host in hosts:
+            node = Node(host)
+            print node.run_list
+            print node.chef_environment
+
             
-            
-            
-        
