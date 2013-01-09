ae2bf077fa9f7dc1efbb5d875f642589f2aaeafd
solo4572 <jacob.wagner@rackspace.com>
2013-01-09 17:09:47 -0600

    added print debugging for display_only flag

--- jenkins/scripts/python/bare_metal/environment/switch_chef_roles.py
+++ jenkins/scripts/python/bare_metal/environment/switch_chef_roles.py
@@ -16,24 +16,23 @@ parser.add_argument('--razor_ip', action="store", dest="razor_ip",
 parser.add_argument('--policy', action="store", dest="policy", 
                     required=True, help="Policy to teardown from razor and reboot nodes")
 
-parser.add_argument('--data-bag-location', action="store", dest="data_bag_loc", 
-                    #default="/home/john/git/rpcsqa/chef-cookbooks/data_bags/razor_node",
+parser.add_argument('--data_bag_location', action="store", dest="data_bag_loc",
                     default="/var/lib/jenkins/rpcsqa/chef-cookbooks/data_bags/razor_node", 
                     required=False, help="Policy to teardown from razor and reboot nodes")
 
-parser.add_argument('--chef-url', action="store", dest="chef_url", 
+parser.add_argument('--chef_url', action="store", dest="chef_url", 
                     default="http://198.101.133.4:4000", 
                     required=False, help="client for chef")
 
-parser.add_argument('--chef-client', action="store", dest="chef_client", 
+parser.add_argument('--chef_client', action="store", dest="chef_client", 
                     default="jenkins", 
                     required=False, help="client for chef")
 
-parser.add_argument('--chef-client-pem', action="store", dest="chef_client_pem", 
+parser.add_argument('--chef_client_pem', action="store", dest="chef_client_pem", 
                     default="/var/lib/jenkins/rpcsqa/.chef/jenkins.pem", 
                     required=False, help="client pem for chef")
 
-parser.add_argument('--display-only', action="store", dest="display_only", 
+parser.add_argument('--display_only', action="store", dest="display_only", 
                     default="true", 
                     required=False, help="Display the node information only (will not reboot or teardown am)")
 
@@ -111,34 +110,30 @@ else:
         root_pass = getrootpass(data)
         dbag_uuid = get_data_bag_UUID(data)
         ip = getip_from_data_bag(dbag_uuid)
-        
-        #Remove active model
-        
-        #SSH into ip and reboot 
-        print "Active Model ID: %s " % active
-        print "Data Bag UUID: %s " % dbag_uuid
-        print "Public address: %s " % ip
-        print "Private address: %s " % private_ip
-        print ""
 
-        if results.display_only == 'false':
-            
-            print "Trying to switch roles and run chef-client for %s...." % ip
-            try:
-                session = ssh_session('root', ip, root_pass, False)
-                host = session.ssh('hostname --fqdn')
-                host = host.replace("\r\n","")
-                print host
-                hosts.append(host)
-            except Exception, e:
-                print "FAILURE: %s " % e
-            finally:
-                session.close()
- 
-    with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
-        for host in hosts:
-            node = Node(host)
-            print node.run_list
-            print node.chef_environment
+        if results.display_only == 'true':
+            print "Active Model ID: %s " % active
+            print "Data Bag UUID: %s " % dbag_uuid
+            print "Public address: %s " % ip
+            print "Private address: %s " % private_ip
+            print ""
 
-            
+        try:
+            session = ssh_session('root', ip, root_pass, False)
+            host = session.ssh('hostname --fqdn')
+            host = host.replace("\r\n","")
+            print host
+            hosts.append(host)
+        except Exception, e:
+            print "FAILURE: %s " % e
+        finally:
+            session.close()
+
+
+        with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
+            for host in hosts:
+                # Get the node from chef
+                node = Node(host)
+
+                if results.display_only == 'true':
+                    print "%s has run list: %s, and environement: %s" % (node, node.run_list, node.chef_environment)
