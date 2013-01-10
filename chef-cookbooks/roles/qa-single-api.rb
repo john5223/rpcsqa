7332f71179cbebaa44840f3ee7201fd77737fa74
solo4572 <jacob.wagner@rackspace.com>
2013-01-10 14:12:38 -0600

    added role qa-single-api

---
+++ chef-cookbooks/roles/qa-single-api.rb
@@ -0,0 +1,6 @@
+name "qa-single-api"
+description "This will create an OpenStack API server for failover for our RPCSQA environment"
+run_list(
+  "role[qa-base]",
+  "recipe[network-interfaces]"
+)
