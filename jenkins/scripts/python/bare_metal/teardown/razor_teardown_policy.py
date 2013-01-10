b5c8a8daaec82a74e2ba692777e05e4759083d15
solo4572 <jacob.wagner@rackspace.com>
2013-01-10 12:12:03 -0600

    still dicking around with chef client via python api

--- jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
+++ jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
@@ -142,7 +142,7 @@ else:
             try:
                 chef_api = ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client)
                 client = chef_api.api_request('GET', '/clients/%s' % chef_name)
-                print "Client: \n %s" % json.dumps(client, indent=4)
+                print "Response: %s Client: \n %s" % (client.code, json.dumps(client, indent=4)
             except Exception, e:
                 print "Error printing chef clients: %s " % e
                 continue
@@ -168,9 +168,8 @@ else:
             print "Searching chef clients..."
             try:
                 chef_api = ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client)
-                client = chef_api.api_request('GET', '/clients/%s' % chef_name) 
-                for k, v in client:
-                    print "Key: %s, Value %s" % (k, v)
+                response = chef_api.api_request('DELETE', '/clients/%s' % chef_name)
+                print "Client %s removed with response: %s" % (chef_name, response.code)
             except Exception, e:
                 print "Error removing chef node: %s " % e
                 continue
