2900a98bf350e1d01e450963d82c29e4445f3c4d
Solomon J. Wagner <swagner1104@gmail.com>
2013-01-11 11:53:12 -0600

    updated roles for compute and controller

--- chef-cookbooks/roles/qa-single-compute.rb
+++ chef-cookbooks/roles/qa-single-compute.rb
@@ -2,5 +2,6 @@ name "qa-single-compute"
 description "This will create an OpenStack compute for RPCS QA Team"
 run_list(
   "role[qa-base]",
-  "recipe[network-interfaces]"
+  "recipe[network-interfaces]",
+  "role[single-compute]"
 )
