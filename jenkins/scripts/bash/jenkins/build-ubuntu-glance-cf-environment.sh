484bc8c89a51f0e12ce228669f1f7d270fbc90a8
solo4572 <jacob.wagner@rackspace.com>
2013-01-10 18:13:55 -0600

    working on script to replace lines

--- jenkins/scripts/bash/jenkins/build-ubuntu-glance-cf-environment.sh
+++ jenkins/scripts/bash/jenkins/build-ubuntu-glance-cf-environment.sh
@@ -35,8 +35,10 @@ cp $template_filename $environment_filename
 
 ## replace the lines we are looking for
 echo "Replacing template values with real values..."
-sed -i 's/<TENANT_ID>/$TENANT_ID/g' $environment_filename
-sed -i 's/<TENANT_NAME>/$TENANT_NAME/g' $environment_filename
-sed -i 's/<TENANT_PASSWORD>/$TENANT_PASSWORD/g' $environment_filename
+sed -i 's/<TENANT_ID>/${TENANT_ID}/g' $environment_filename
+sed -i 's/<TENANT_NAME>/${TENANT_NAME}/g' $environment_filename
+sed -i 's/<TENANT_PASSWORD>/${TENANT_PASSWORD}/g' $environment_filename
+
+echo "Set Knife Environment..."
 
 echo "Done..."
