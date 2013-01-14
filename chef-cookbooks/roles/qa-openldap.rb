acd4f546e0542d974258c7bbbd26015471fd852b
john5223 <john.curran@rackspace.com>
2013-01-14 10:28:09 -0600

    Added network-interfaces to qa-openldap role

--- chef-cookbooks/roles/qa-openldap.rb
+++ chef-cookbooks/roles/qa-openldap.rb
@@ -2,5 +2,6 @@ name "qa-openldap"
 description "This will create an openldap server for RPCS QA Team"
 run_list(
   "role[qa-base]",
+  "recipe[network-interfaces]",
   "recipe[openldap::server]"
 )
