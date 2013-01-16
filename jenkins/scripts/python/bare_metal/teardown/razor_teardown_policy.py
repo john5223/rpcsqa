7f592d550f3c539108595198736871988d7a4987
solo4572 <jacob.wagner@rackspace.com>
2013-01-16 12:18:06 -0600

    trying to chenge ip stuff

--- jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
+++ jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
@@ -132,8 +132,8 @@ else:
             chef_node = chef_api.api_request('GET', '/nodes/%s' % chef_name)
             chef_client = chef_api.api_request('GET', '/clients/%s' % chef_name)
             # if chef has a node for this box, change the ip to the ip that chef has for it
-            print json.dumps(chef_node, indent=4)
-            #ip = chef_node['ipaddress']
+            #print json.dumps(chef_node, indent=4)
+            ip = chef_node['ipaddress']
         except Exception, e:
             print "Razor node %s doesnt have a chef node / chef client, exception %s" % (chef_name, e)
             continue
