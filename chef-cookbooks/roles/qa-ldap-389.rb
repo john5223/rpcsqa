542b625fe9878a2c2b8f009e34144a51ca5929f3
john5223 <john.curran@rackspace.com>
2013-01-22 10:44:41 -0600

    Removed unncessary attribs  from qa-ldap-389 role

--- chef-cookbooks/roles/qa-ldap-389.rb
+++ chef-cookbooks/roles/qa-ldap-389.rb
@@ -7,10 +7,7 @@ run_list(
 )
 default_attributes(  { "389" => {
                             "rootpw" => "{SSHA}gqx00u6a46YT0zsIC9nQVy2yyHfNjJmO", 
-                            "basedn" => "dc=dev,dc=rcbops,dc=me",
-                            "server" => "ldap.rcbops",
-                            "auth_bindpw" => "{SSHA}gqx00u6a46YT0zsIC9nQVy2yyHfNjJmO",
-                            "slapd_type" => "master"
+                            "basedn" => "dc=dev,dc=rcbops,dc=me"
                             }   
                       }
                   )
