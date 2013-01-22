fac4f59455a12728bd5cfd7b7cd4291d84eb8f23
john5223 <john.curran@rackspace.com>
2013-01-22 09:34:26 -0600

    Added 389 cookbook and role.

--- chef-cookbooks/roles/qa-389.rb
+++ chef-cookbooks/roles/qa-389.rb
@@ -1,11 +1,11 @@
-name "qa-openldap"
-description "This will create an openldap server for RPCS QA Team"
+name "qa-389"
+description "This will create an 389 ldap server for RPCS QA Team"
 run_list(
   #"role[qa-base]",
   #"recipe[network-interfaces]",
-  "recipe[openldap::server]"
+  "recipe[389::server]"
 )
-default_attributes(  { "openldap" => {
+default_attributes(  { "389" => {
                             "rootpw" => "{SSHA}gqx00u6a46YT0zsIC9nQVy2yyHfNjJmO", 
                             "basedn" => "dc=dev,dc=rcbops,dc=me",
                             "server" => "ldap.rcbops",
