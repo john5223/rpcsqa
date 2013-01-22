b93cc72b140079f5aa39846eea93fcf2b4c769b3
solo4572 <jacob.wagner@rackspace.com>
2013-01-22 16:07:07 -0600

    updated all in one recipe

--- chef-cookbooks/roles/qa-all-in-one.rb
+++ chef-cookbooks/roles/qa-all-in-one.rb
@@ -1,6 +1,7 @@
 name "qa-all-in-one"
 description "This will create an all-in-one Openstack cluster for RPCS QA Team"
 run_list(
-  "role[qa-base]",
-  "recipe[network-interfaces]"
+  "recipe[razor]",
+  "recipe[network-interfaces]",
+  "role[allinone]"
 )
