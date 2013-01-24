b66729afaf5ebd2d145c7b5833f984027fc5105c
john5223 <john.curran@rackspace.com>
2013-01-24 11:35:17 -0600

    Working on openldap environment in jenkins.

--- jenkins/scripts/bash/jenkins/build-ubuntu-keystone-ldap-environment.sh
+++ jenkins/scripts/bash/jenkins/build-ubuntu-keystone-ldap-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source the file that has our environment variables
-#source ~/source_files/CLOUD_FILES_AUTH.sh
+#source ~/source_files/LDAP.sh
 
 template_filename='/var/lib/jenkins/rpcsqa/chef-cookbooks/environments/templates/ubuntu-keystone-ldap.json'
 environment_filename='/var/lib/jenkins/rpcsqa/chef-cookbooks/environments/ubuntu-keystone-ldap.json'
@@ -10,6 +10,13 @@ filelines=`cat $filename`
 ## copy the environment file to the proper directory
 echo "Copying template to environment..."
 cp $template_filename $environment_filename
+
+
+## replace the lines we are looking for
+echo "Replacing template values with real values..."
+result=`sed -i 's/<LDAP_IP>/'${LDAP_IP}'/g' $environment_filename`
+result=`sed -i 's/<LDAP_ADMIN_PASS>/'${LDAP_ADMIN_PASS}'/g' $environment_filename`
+
 
 echo "Set Knife Environment..."
 knife environment from file $environment_filename
