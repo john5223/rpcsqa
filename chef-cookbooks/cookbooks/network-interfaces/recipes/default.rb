5100e4b56aff5d272535afb357d4d49c83c4e8ff
solo4572 <jacob.wagner@rackspace.com>
2013-01-24 11:35:58 -0600

    printing debugging added

--- chef-cookbooks/cookbooks/network-interfaces/recipes/default.rb
+++ chef-cookbooks/cookbooks/network-interfaces/recipes/default.rb
@@ -91,8 +91,9 @@ case node['platform']
         puts "scripts directory: #{iface_scripts_dir}"
         Dir.chdir("#{iface_scripts_dir}")
         puts Dir.pwd
-        all_iface_files = Dir.glob("^ifcfg-*$")
-        puts "iface files: #{all_iface_files}"
+        all_iface_files = Dir.glob("ifcfg-*")
+        all_iface_files.each do | iface_file |
+          puts "File: #{iface_file}"
       end
     end
 
