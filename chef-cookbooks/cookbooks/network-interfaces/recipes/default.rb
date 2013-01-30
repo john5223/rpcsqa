95caa44851150d20b5d47be73a9672521e2e9c6f
solo4572 <jacob.wagner@rackspace.com>
2013-01-30 13:15:14 -0600

    printing debug to find key value diff

--- chef-cookbooks/cookbooks/network-interfaces/recipes/default.rb
+++ chef-cookbooks/cookbooks/network-interfaces/recipes/default.rb
@@ -144,7 +144,8 @@ case node['platform']
               change = false
               node_iface.each_pair do | k, v |
                 if file_hash["#{k.upcase}"].nil? || file_hash["#{k.upcase}"] != "#{v}"
-                  puts "Found a diff in #{ifcfg_file}: key: #{k}, value #{v}."
+                  puts "Found a diff in #{ifcfg_file}"
+                  puts "Current key: #{k.upcase} value: #{file_hash['k']} will be changed to #{v}"
                   file_hash["#{k.upcase}"] = "\"#{v}\"\n"
                   change = true
                 end
