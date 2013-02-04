5f94ccfc04da59071e539de713044930701c18d9
solo4572 <jacob.wagner@rackspace.com>
2013-02-04 16:44:06 -0600

    changed to use a sourced file

--- jenkins/scripts/bash/razor/ping-reboot-all.sh
+++ jenkins/scripts/bash/razor/ping-reboot-all.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+# Source the file that has our environment variables
+source ~/source_files/HELLO_WORLD.sh
+
 # print usage and exit
-if [ "$#" -eq 0 ]; then
-        echo "Usage: ping_reboot_all.sh -p root_pass" >&2
-        exit
-fi
+#if [ "$#" -eq 0 ]; then
+#        echo "Usage: ping_reboot_all.sh -p root_pass" >&2
+#        exit
+#fi
+
 
 # Get the root password from the box off the command line
-while getopts "p:h" OPTION;
-do
-        case $OPTION in
-                p) ROOT_PASS=$OPTARG
-                   ;;
-                h) echo "Usage: nmap_reboot.sh [-h]" >&2
-                   echo " -h Return this help information" >&2
-                   echo " -p The root password for the boxes to be rebooted" >&2
-                   exit
-                   ;;
-        esac
-done
+#while getopts "p:h" OPTION;
+#do
+#        case $OPTION in
+#                p) ROOT_PASS=$OPTARG
+#                   ;;
+#                h) echo "Usage: nmap_reboot.sh [-h]" >&2
+#                   echo " -h Return this help information" >&2
+#                   echo " -p The root password for the boxes to be rebooted" >&2
+#                   exit
+#                   ;;
+#        esac
+#done
 
 # ping all ips in range and reboot the good ones
 for i in {2..254}
@@ -30,7 +34,7 @@ do
 
         if [[ ${#results} > 300 ]]; then
                 echo "!!## -- Rebooting box with ip: $ip -- ##!!"
-                sshpass -p $ROOT_PASS ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root $ip 'reboot'
+                sshpass -p $PASS ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root $ip 'reboot'
         else
                 echo "!!## -- Nothing @ $ip to reboot -- ##!!"
         fi
