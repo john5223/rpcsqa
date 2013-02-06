e726a21c493a3b4773afcb14892ab13e87e3a37b
solo4572 <jacob.wagner@rackspace.com>
2013-02-06 13:46:02 -0600

    made the script use the source file instead of parameters

--- jenkins/scripts/bash/razor/nmap-reboot.sh
+++ jenkins/scripts/bash/razor/nmap-reboot.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# print usage and exit
-if [ "$#" -eq 0 ]; then
-        echo "Usage: namp_reboot.sh -p root_pass" >&2
-        exit
-fi
+# Source the file that has our environment variables
+source ~/source_files/HELLO_WORLD.sh
 
-# Get the root password from the box off the command line
-while getopts "p:h" OPTION;
-do
-        case $OPTION in
-                p) ROOT_PASS=$OPTARG
-                   ;;
-                h) echo "Usage: nmap_reboot.sh [-h]" >&2
-                   echo " -h Return this help information" >&2
-                   echo " -p The root password for the boxes to be rebooted" >&2
-                   exit
-                   ;;
-        esac
-done
+
+# print usage and exit
+#if [ "$#" -eq 0 ]; then
+        #echo "Usage: namp_reboot.sh -p root_pass" >&2
+        #exit
+#fi
+#
+## Get the root password from the box off the command line
+#while getopts "p:h" OPTION;
+#do
+        #case $OPTION in
+                #p) ROOT_PASS=$OPTARG
+                   #;;
+                #h) echo "Usage: nmap_reboot.sh [-h]" >&2
+                   #echo " -h Return this help information" >&2
+                   #echo " -p The root password for the boxes to be rebooted" >&2
+                   #exit
+                   #;;
+        #esac
+#done
 
 # Run nmap to get the boxes that are alive
 results=`nmap -sP -oG alive 10.0.0.0/24 | grep 10.0.0.* | awk '{print $5 $6}'`
@@ -33,7 +37,7 @@ do
                         echo "This box is restricted infrastructure, ignore it."
                 else
                         echo "Rebooting machine with ip $ip"
-                        sshpass -p $ROOT_PASS ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root $ip 'reboot'
+                        sshpass -p $PASS ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root $ip 'reboot'
                 fi
         fi
 done
