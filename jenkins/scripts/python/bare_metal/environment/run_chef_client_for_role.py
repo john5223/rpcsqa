3827a3f93c17d125213632ddde5a7fcb27f49ff0
solo4572 <jacob.wagner@rackspace.com>
2013-02-15 13:48:14 -0600

    escaping the single quotes

--- jenkins/scripts/python/bare_metal/environment/run_chef_client_for_role.py
+++ jenkins/scripts/python/bare_metal/environment/run_chef_client_for_role.py
@@ -95,10 +95,10 @@ else:
                     if server['platform_family'] == 'rhel':
                         print "Commenting out requiretty..."
                         try:
-                            sed_regex = "s/^Defaults[ ]+requiretty/#Defaults requiretty/g"
-                            sed_string = "sed -i -E '%s' /etc/sudoers" % sed_regex
+                            sed_regex = "s/^Defaults[ ]\+requiretty/#Defaults requiretty/g"
+                            sed_string = "sed -i -e '%s' /etc/sudoers" % sed_regex
                             print "SED STRING: %s" % sed_string
-                            return_code = subprocess.check_output("sshpass -p %s ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root %s '%s'" % (server['root_password'], server['ip'], sed_string), stderr=subprocess.STDOUT, shell=True)
+                            return_code = subprocess.check_output("sshpass -p %s ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l root %s \'%s\'" % (server['root_password'], server['ip'], sed_string), stderr=subprocess.STDOUT, shell=True)
                             print "Successfully commented out requiretty..."
                         except Exception, e:
                             print "Failed to comment out requiretty..."
