7f226c3c049c4b15464d6c3cef561e46f7f28e8f
Cameron Lopez <cameronjlopez@gmail.com>
2013-02-25 18:06:02 -0600

    debug nodes

--- jenkins/scripts/python/bare_metal/environment/build_supernova_config.py
+++ jenkins/scripts/python/bare_metal/environment/build_supernova_config.py
@@ -43,7 +43,7 @@ print "!!## -- Display only: %s -- ##!!" % results.display_only
 with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
     nodes = Role(role).list
     print type(nodes)
-
+    print nodes
     for node in nodes:
         env_name = node.chef_environment
         print "Saving environment for environment: " + node.chef_environment
