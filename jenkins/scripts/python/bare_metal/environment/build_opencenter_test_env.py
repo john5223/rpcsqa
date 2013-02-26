5e09718058c2333b408b11c61d78b8c7326e08aa
Cameron Lopez <cameronjlopez@gmail.com>
2013-02-26 14:53:27 -0600

    back to roles

--- jenkins/scripts/python/bare_metal/environment/build_opencenter_test_env.py
+++ jenkins/scripts/python/bare_metal/environment/build_opencenter_test_env.py
@@ -90,12 +90,12 @@ if active_models:
         for server in servers:
             # If the role is opencenter-server, save the server information.
             print server['run_list']
-            if 'qa-opencenter-server' in server['run_list']:
+            if 'role[qa-opencenter-server]' in server['run_list']:
                 opencenter_test_env['INSTANCE_SERVER_HOSTNAME'] = server['node']
                 opencenter_server_ip = server['ip']
                 opencenter_server_password = server['root_password']
             # if the role is opencenter-client, add to temp list.
-            elif 'qa-opencenter-agent' in server['run_list']:
+            elif 'role[qa-opencenter-agent]' in server['run_list']:
                 client_temp.append(server['node'])
                 print "appending to client temp: %s" % server['node']
             else:
