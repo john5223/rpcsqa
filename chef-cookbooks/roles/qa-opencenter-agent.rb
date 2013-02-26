d93b6ff67509c044ada94302fbfb5e8020135042
solo4572 <jacob.wagner@rackspace.com>
2013-02-26 12:11:38 -0600

    renamed client to agent in scripts and roles

---
+++ chef-cookbooks/roles/qa-opencenter-agent.rb
@@ -0,0 +1,5 @@
+name "qa-opencenter-agent"
+description "This will create a opencenter qa-opencenter-agent for the RPCSQA Team"
+run_list(
+  "recipe[network-interfaces]"
+)
--- chef-cookbooks/roles/qa-opencenter-client.rb
+++
@@ -1,5 +0,0 @@
-name "qa-opencenter-client"
-description "This will create a opencenter client for the RPCSQA Team"
-run_list(
-  "recipe[network-interfaces]"
-)
