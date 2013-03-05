108c4eea746622a3206647dd7800bb421487d343
solo4572 <jacob.wagner@rackspace.com>
2013-03-05 12:34:40 -0600

    printing the interfaces

--- jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
+++ jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
@@ -125,7 +125,9 @@ if active_models:
                             if 'em2' in interface:
                                 addresses = node['network']['interfaces']['%s' % interface]['addresses'].iteritems()
                                 for k, v in addresses:
-                                    print "Key: %s -- Value: %s" % (k , v)
+                                    if 'inet' in addresses[k].items()
+                                        print "!!## -- Private IP: %s" % addresses[k]
+                                        break
                         else:
                             print "Platform not supported..."
                     print "!!## -- Node found %s, has ip %s -- ##!!" % (chef_name, ip)
