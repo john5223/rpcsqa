51bc7ca6729a10af6cb7aea771cc9cdf80e89ece
solo4572 <jacob.wagner@rackspace.com>
2013-03-05 13:02:59 -0600

    printing the interfaces

--- jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
+++ jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
@@ -82,7 +82,7 @@ def get_private_ip(chef_node_addresses):
         print "!!## -- Key: %s -- Value: %s -- ##!!" % (k,v)
         for k2, v2 in v.iteritems():
             print "!!## -- Key2: %s type(%s) -- Value2: %s type(%s) -- ##!!" % (k2, type(k2), v2, type(v2))
-            if v2 is 'inet':
+            if str(v2) is 'inet':
                 print "!!## -- Private IP: %s -- ##!!" % k
                 return k
 
