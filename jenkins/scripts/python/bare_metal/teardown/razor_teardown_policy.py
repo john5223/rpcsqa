75ef0ef90dc9488c8a944ac5b7ea8a025334dccb
solo4572 <jacob.wagner@rackspace.com>
2013-03-05 12:24:31 -0600

    printing the interfaces

--- jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
+++ jenkins/scripts/python/bare_metal/teardown/razor_teardown_policy.py
@@ -119,11 +119,13 @@ if active_models:
                         if platform_family == 'debian':
                             if 'eth1' in interface:
                                 for address in node['network']['interfaces']['%s' % interface]['addresses'].iteritems():
-                                    print address[0]
+                                    if 'inet' in address['family']:
+                                        print json.dumps(address[0], indent=4)
                         elif platform_family == 'rhel':
                             if 'em2' in interface:
                                 for address in node['network']['interfaces']['%s' % interface]['addresses'].iteritems():
-                                    print address[0]
+                                    if 'inet' in address['family']
+                                        print json.dumps(address[0], indent=4)
                         else:
                             print "Platform not supported..."
                     print "!!## -- Node found %s, has ip %s -- ##!!" % (chef_name, ip)
