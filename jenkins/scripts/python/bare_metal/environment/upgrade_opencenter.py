363313e7feee5c7f10d738ff281646ad6c9d42df
Cameron Lopez <cameronjlopez@gmail.com>
2013-03-26 11:10:09 -0500

    done update, only edit the repo list, waiting for centos procedures

--- jenkins/scripts/python/bare_metal/environment/upgrade_opencenter.py
+++ jenkins/scripts/python/bare_metal/environment/upgrade_opencenter.py
@@ -6,6 +6,8 @@ from chef import *
 from razor_api import razor_api
 from subprocess import check_call, CalledProcessError
 
+default_repo_centos = "http://build.monkeypuppetlabs.com/repo-testing/RedHat/6/$basearch/"
+
 # Parse arguments from the cmd line
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', action="store", dest="name", required=False, default="test", 
@@ -16,7 +18,11 @@ parser.add_argument('--os', action="store", dest="os", required=False, default='
 
 parser.add_argument('--repo_url', action="store", dest="repo", required=False, 
                     default="http://build.monkeypuppetlabs.com/proposed-packages/rcb-utils", 
-                    help="URL of the OpenCenter install scripts")
+                    help="URL of the OpenCenter package repo")
+
+parser.add_argument('--key', action="store", dest="key", required=False, 
+                    default="http://build.monkeypuppetlabs.com/repo-testing/RPM-GPG-RCB.key", 
+                    help="URL of the OpenCenter package repo gpgkey")
 
 #Defaulted arguments
 parser.add_argument('--razor_ip', action="store", dest="razor_ip", default="198.101.133.3",
@@ -41,25 +47,31 @@ def run_remote_ssh_cmd(server_ip, user, passwd, remote_cmd):
         return {'success': False, 'return': None, 'exception': cpe, 'command': command}
 
 """
-Find opencenter agents and upload their packages
+Find opencenter agents and update their repos
 """
 with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
     razor = razor_api(results.razor_ip)
     servers = []
     env = "%s-%s-opencenter" % (results.name, results.os)
     nodes = Search('node').query("name:qa-%s-pool* AND chef_environment:%s" % (results.os, env))
-    package = "deb %s precise rcb-utils" % results.repo
-    command = \
-"""
-sed -i 's/(.*)/#\1/g' /etc/apt/sources.list.d/rcb-utils.list
-echo '%s' >> /etc/apt/sources.list.d/rcb-utils.list
-apt-get update
-apt-get upgrade -y
-""" % package
     
-    print "Running command: %s\n On servers for environment: %s" % (command, env)
+
+    # Use the correct command to upgrade packages
+    if results.os == "ubuntu":  # Ubuntu upgrade
+        package = "deb %s precise rcb-utils" % results.repo
+        commands = ["sed -i 's/\(.*\)/#\1/g' /etc/apt/sources.list.d/rcb-utils.list",
+                    "echo '%s' >> /etc/apt/sources.list.d/rcb-utils.list" % package ]
+    else:                       
+        commands = ["""echo "[rcb-utils]
+name=RCB Utility packages for OpenCenter CentOS
+baseurl=%s
+enabled=1
+gpgcheck=1
+gpgkey=%s" > /etc/yum.repos.d/rcb-utils.repo""" % (default_repo_centos, results.key)]
     
     for n in nodes:
         node = Node(n['name'])
         password = razor.get_active_model_pass(node.attributes['razor_metadata']['razor_active_model_uuid'])['password']
-        run_remote_ssh_cmd(node['ipaddress'], 'root', password, command)
+        for command in commands: 
+            print "Running command: %s\n On server: %s for environment: %s" % (command, node['ipaddress'], env)
+            run_remote_ssh_cmd(node['ipaddress'], 'root', password, command)
