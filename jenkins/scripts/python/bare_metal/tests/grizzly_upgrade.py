465908570ffa34781f4e65ec623e6244f35b312f
Cameron Lopez <cameronjlopez@gmail.com>
2013-04-10 15:47:20 -0500

    correctly access ipaddress

--- jenkins/scripts/python/bare_metal/tests/grizzly_upgrade.py
+++ jenkins/scripts/python/bare_metal/tests/grizzly_upgrade.py
@@ -66,8 +66,8 @@ with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
         print "environment %s not found" % env
         sys.exit(1)
     query = "in_use:\"server\" AND chef_environment:%s" % env
-    opencenter_server_ip = Search('node').query(query).attributes['ipaddress'],
-    ep = OpenCenterEndpoint("https://%s:8443" % opencenter_server_ip,
+    opencenter_server_ip = next(Search('node').query(query)).attributes['ipaddress']
+    ep = OpenCenterEndpoint("https://%s:8443" % opencenter_server
                             user="admin",
                             password="password")
     chef_envs = []
