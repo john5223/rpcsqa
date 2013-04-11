07f51bd399b574eb3c7512bc8cd3ef3a572a22e5
Cameron Lopez <cameronjlopez@gmail.com>
2013-04-11 15:57:04 -0500

    useful output

--- jenkins/scripts/python/bare_metal/tests/grizzly_upgrade.py
+++ jenkins/scripts/python/bare_metal/tests/grizzly_upgrade.py
@@ -41,6 +41,7 @@ results = parser.parse_args()
 def run_remote_ssh_cmd(server_ip, user, passwd, remote_cmd):
     """Runs a command over an ssh connection"""
     command = "sshpass -p %s ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l %s %s '%s'" % (passwd, user, server_ip, remote_cmd)
+    print "##### Running command: %s #####" % remote_cmd
     try:
         ret = check_call(command, shell=True)
         return {'success': True, 'return': ret, 'exception': None}
