d42a2133e712f11b6e6a3013f18befeebf4201f9
Cameron Lopez <cameronjlopez@gmail.com>
2013-04-13 12:11:14 -0500

    remove tempest from test_opencenter

--- jenkins/scripts/python/bare_metal/tests/test_opencenter.py
+++ jenkins/scripts/python/bare_metal/tests/test_opencenter.py
@@ -3,10 +3,10 @@ import os
 import sys
 import requests
 import argparse
-import json
 from chef import ChefAPI, Search, Node, Environment
 from razor_api import razor_api
 from subprocess import check_call, CalledProcessError
+from server_helper import run_remote_ssh_cmd
 
 # Parse arguments from the cmd line
 parser = argparse.ArgumentParser()
@@ -28,15 +28,7 @@ parser.add_argument('--HA', action="store", dest="HA", required=False,
 parser.add_argument('--tempest', action="store", dest="tempest",
                     required=False, default=False,
                     help="Run tempest on openstack cluster")
-parser.add_argument('--tempest_dir', action="store", dest="tempest_dir",
-                    required=False,
-                    default="/var/lib/jenkins/tempest/folsom/tempest")
-parser.add_argument('--tempest_version', action="store",
-                    dest="tempest_version", required=False,
-                    default="folsom")
-parser.add_argument('--keystone_admin_pass', action="store",
-                    dest="keystone_admin_pass", required=False,
-                    default="secrete")
+
 
 #Defaulted arguments
 parser.add_argument('--razor_ip', action="store", dest="razor_ip",
@@ -58,23 +50,6 @@ if results.HA == "true":
     results.HA = True
 elif results.HA == "false":
     results.HA = False
-if results.tempest == "true":
-    results.tempest = True
-elif results.tempest == "false":
-    results.tempest = False
-
-
-def run_remote_ssh_cmd(server_ip, user, passwd, remote_cmd):
-    """Runs a command over an ssh connection"""
-    command = "sshpass -p %s ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -l %s %s '%s'" % (passwd, user, server_ip, remote_cmd)
-    try:
-        ret = check_call(command, shell=True)
-        return {'success': True, 'return': ret, 'exception': None}
-    except CalledProcessError, cpe:
-        return {'success': False,
-                'retrun': None,
-                'exception': cpe,
-                'command': command}
 
 # Load chef and razor apis
 with ChefAPI(results.chef_url, results.chef_client_pem, results.chef_client):
@@ -252,90 +227,5 @@ nova_mysql_vip = %s
             print "!!## -- Command %s failed to run on server with ip: %s -- ##!!" % (command, opencenter_server_ip)
             print "!!## -- Return Code: %s -- ##!!" % cpe.returncode
             #print "!!## -- Command: %s -- ##!!" % cpe.cmd
-            print "!!## -- Output: %s -- ##!!" % cpe.output
-            sys.exit(1)
-    
-    # Run tempest on cluster
-    if results.tempest:
-        # Gather information of cluster
-        if results.HA:
-            ip = vip_data['nova_api_vip']
-        else:
-            ip = Node(agents[0])['ipaddress']
-
-        url = "http://%s:5000/v2.0" % ip
-        token_url = "%s/tokens" % url
-
-        # Gather cluster information from the cluster
-        auth = {
-            'auth': {
-                'tenantName': 'admin',
-                'passwordCredentials': {
-                    'username': 'admin',
-                    'password': '%s' % results.keystone_admin_pass
-                }
-            }
-        }
-
-        image_id = None
-        image_alt = None
-        try:
-            r = requests.post(token_url, data=json.dumps(auth),
-                              headers={'Content-type': 'application/json'})
-            ans = json.loads(r.text)
-            if 'access' in ans.keys():
-                token = ans['access']['token']['id']
-                images_url = "http://%s:9292/v2/images" % ip
-                images = json.loads(requests.get(images_url,
-                                    headers={'X-Auth-Token': token}).text)
-                image_ids = (image['id'] for image in images['images'])
-                image_id = next(image_ids)
-                image_alt = next(image_ids) or image_id
-        except Exception, e:
-            print "Failed to add keystone info. Exception: %s" % e
-            sys.exit(1)
-
-        # Write the config
-        try:
-            sample_path = "%s/etc/base_%s.conf" % (results.tempest_dir,
-                                                   results.tempest_version)
-            with open(sample_path) as f:
-                sample_config = f.read()
-           
-            tempest_config = str(sample_config)
-            tempest_config = tempest_config.replace('http://127.0.0.1:5000/v2.0/',
-                                                    url)
-            tempest_config = tempest_config.replace('{$KEYSTONE_IP}', ip)
-            tempest_config = tempest_config.replace('localhost', ip)
-            tempest_config = tempest_config.replace('127.0.0.1', ip)
-            tempest_config = tempest_config.replace('{$IMAGE_ID}', image_id)
-            tempest_config = tempest_config.replace('{$IMAGE_ID_ALT}',
-                                                    image_alt)
-            tempest_config = tempest_config.replace('ostackdemo',
-                                                    results.keystone_admin_pass)
-            tempest_config = tempest_config.replace('demo', "admin")
-           
-            tempest_config_path = "%s/etc/%s-%s.conf" % \
-                                  (results.tempest_dir, results.name,
-                                   results.os)
-            with open(tempest_config_path, 'w') as w:
-                print "####### Tempest Config #######"
-                print tempest_config_path
-                print tempest_config
-                w.write(tempest_config)
-           
-        except Exception as e:
-            print "Failed writing tempest config, exception: %s" % e
-            sys.exit(1)
-
-        # Run tests
-        try:
-            print "!! ## -- Running tempest -- ## !!"
-            check_call_return = check_call(
-                "export TEMPEST_CONFIG=%s; python -u /usr/local/bin/nosetests %s/tempest/tests/compute" % (tempest_config_path, results.tempest_dir), shell=True)
-            print "!!## -- Tempest tests ran successfully  -- ##!!"
-        except CalledProcessError, cpe:
-            print "!!## -- Tempest tests failed -- ##!!"
-            print "!!## -- Return Code: %s -- ##!!" % cpe.returncode
             print "!!## -- Output: %s -- ##!!" % cpe.output
             sys.exit(1)
