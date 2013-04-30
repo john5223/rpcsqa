d26dfc8267077c217fbe0a8b30b98b88518bf04f
solo4572 <jacob.wagner@rackspace.com>
2013-04-30 16:53:23 -0500

    more refactoring work

--- jenkins/modules/bare-metal/rpcsqa_helper.py
+++ jenkins/modules/bare-metal/rpcsqa_helper.py
@@ -172,7 +172,7 @@ class rpcsqa_helper:
         """
         @param chef_environment
         """
-        nodes = Search('node').query("chef_environment:%s AND NOT in_use:0") % chef_environment
+        nodes = Search('node').query("chef_environment:%s AND NOT in_use:0" % chef_environment)
         for n in nodes:
             erase_node(n)
 
