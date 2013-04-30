5004f5faf5a469b41e6ef18d71fe1bb7e915c755
solo4572 <jacob.wagner@rackspace.com>
2013-04-30 17:20:45 -0500

    more refactoring work

--- jenkins/modules/bare-metal/rpcsqa_helper.py
+++ jenkins/modules/bare-metal/rpcsqa_helper.py
@@ -174,7 +174,7 @@ class rpcsqa_helper:
         """
         nodes = Search('node').query("chef_environment:%s AND NOT in_use:0" % chef_environment)
         for n in nodes:
-            self.erase_node(n)
+            self.erase_node(n['name'])
 
     def clone_git_repo(self, server, github_user, github_pass):
         chef_node = Node(server)
