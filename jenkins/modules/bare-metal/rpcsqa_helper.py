4426ec6d17577f71a019ea7253844de16fe7186f
solo4572 <jacob.wagner@rackspace.com>
2013-05-01 10:33:03 -0500

    alpha rpcsqa helper

--- jenkins/modules/bare-metal/rpcsqa_helper.py
+++ jenkins/modules/bare-metal/rpcsqa_helper.py
@@ -21,6 +21,64 @@ class rpcsqa_helper:
         
         return outl
 
+    def build_dir_server(self, dir_node):
+        # We dont support 389 yet, so exit if it is not ldap
+        if results.dir_version != 'openldap':
+            print "%s as a directory service is not yet supported...exiting" % results.dir_version
+            sys.exit(1)
+
+        # Build directory service node
+        ip = dir_node['ipaddress']
+        root_pass = self.razor_password(dir_node)
+        dir_node['in_use'] = 'directory-server'
+        dir_node.run_list = ["role[qa-%s-%s]" % (results.dir_version, results.os)]
+        dir_node.save()
+
+        print "Updating server...this may take some time"
+        update_node(dir_node)
+
+        # if redhat platform, disable iptables
+        if dir_node['platform_family'] == 'rhel':
+            print "Platform is RHEL family, disabling iptables"
+            disable_iptables(dir_node)
+
+        # Run chef-client twice
+        print "Running chef-client for directory service node...this may take some time..."
+        run1 = run_chef_client(dir_node)
+        if run1['success']:
+            print "First chef-client run successful...starting second run..."
+            run2 = run_chef_client(dir_node)
+            if run2['success']:
+                print "Second chef-client run successful..."
+            else:
+                print "Error running chef-client for directory node %s" % dir_node
+                print run2
+                sys.exit(1)
+        else:
+            print "Error running chef-client for directory node %s" % dir_node
+            print run1
+            sys.exit(1)
+
+        # Directory service is set up, need to import config
+        if run1['success'] and run2['success']:
+            if results.dir_version == 'openldap':
+                scp_run = run_remote_scp_cmd(ip, 'root', root_pass, '/var/lib/jenkins/source_files/ldif/*.ldif')
+                if scp_run['success']:
+                    ssh_run = run_remote_ssh_cmd(ip, 'root', root_pass, 'ldapadd -x -D \"cn=admin,dc=dev,dc=rcbops,dc=me\" -f base.ldif -w@privatecloud')
+            elif results.dir_version == '389':
+                # Once we support 389, code here to import needed config files
+                print "389 is not yet supported..."
+                sys.exit(1)
+            else:
+                print "%s is not supported...exiting" % results.dir_version
+                sys.exit(1)
+
+        if scp_run['success'] and ssh_run['success']:
+            print "Directory Service: %s successfully set up..." % results.dir_version
+        else:
+            print "Failed to set-up Directory Service: %s..." % results.dir_version
+            sys.exit(1)
+
     def build_computes(self, computes):
         # Run computes
         print "Making the compute nodes..."
@@ -92,81 +150,12 @@ class rpcsqa_helper:
             print run1
             sys.exit(1)
 
-    def build_dir_server(self, dir_node):
-        # We dont support 389 yet, so exit if it is not ldap
-        if results.dir_version != 'openldap':
-            print "%s as a directory service is not yet supported...exiting" % results.dir_version
-            sys.exit(1)
-
-        # Build directory service node
-        ip = dir_node['ipaddress']
-        root_pass = self.razor_password(dir_node)
-        dir_node['in_use'] = 'directory-server'
-        dir_node.run_list = ["role[qa-%s-%s]" % (results.dir_version, results.os)]
-        dir_node.save()
-
-        print "Updating server...this may take some time"
-        update_node(dir_node)
-
-        # if redhat platform, disable iptables
-        if dir_node['platform_family'] == 'rhel':
-            print "Platform is RHEL family, disabling iptables"
-            disable_iptables(dir_node)
-
-        # Run chef-client twice
-        print "Running chef-client for directory service node...this may take some time..."
-        run1 = run_chef_client(dir_node)
-        if run1['success']:
-            print "First chef-client run successful...starting second run..."
-            run2 = run_chef_client(dir_node)
-            if run2['success']:
-                print "Second chef-client run successful..."
-            else:
-                print "Error running chef-client for directory node %s" % dir_node
-                print run2
-                sys.exit(1)
-        else:
-            print "Error running chef-client for directory node %s" % dir_node
-            print run1
-            sys.exit(1)
-
-        # Directory service is set up, need to import config
-        if run1['success'] and run2['success']:
-            if results.dir_version == 'openldap':
-                scp_run = run_remote_scp_cmd(ip, 'root', root_pass, '/var/lib/jenkins/source_files/ldif/*.ldif')
-                if scp_run['success']:
-                    ssh_run = run_remote_ssh_cmd(ip, 'root', root_pass, 'ldapadd -x -D \"cn=admin,dc=dev,dc=rcbops,dc=me\" -f base.ldif -w@privatecloud')
-            elif results.dir_version == '389':
-                # Once we support 389, code here to import needed config files
-                print "389 is not yet supported..."
-                sys.exit(1)
-            else:
-                print "%s is not supported...exiting" % results.dir_version
-                sys.exit(1)
-
-        if scp_run['success'] and ssh_run['success']:
-            print "Directory Service: %s successfully set up..." % results.dir_version
-        else:
-            print "Failed to set-up Directory Service: %s..." % results.dir_version
-            sys.exit(1)
-
     def check_cluster_size(self, chef_nodes, size):
         if len(chef_nodes) < size:
             print "*****************************************************"
             print "Not enough nodes for the cluster_size given: %s " % cluster_size
             print "*****************************************************"
             sys.exit(1)
-
-    def clear_pool(self, chef_nodes, environment):
-        for n in chef_nodes:
-            name = n['name']
-            node = Node(name)
-            if node.chef_environment == environment:
-                if node['in_use'] != 0:
-                    self.erase_node(node)
-                else:
-                    node.chef_environment = "_default"
-                    node.save()
 
     def cleanup_environment(self, chef_environment):
         """
@@ -181,6 +170,17 @@ class rpcsqa_helper:
             else:
                 node.chef_environment = "_default"
                 node.save()
+
+    def clear_pool(self, chef_nodes, environment):
+        for n in chef_nodes:
+            name = n['name']
+            node = Node(name)
+            if node.chef_environment == environment:
+                if node['in_use'] != 0:
+                    self.erase_node(node)
+                else:
+                    node.chef_environment = "_default"
+                    node.save()
 
     def clone_git_repo(self, server, github_user, github_pass):
         chef_node = Node(server)
@@ -209,6 +209,17 @@ class rpcsqa_helper:
         root_pass = self.razor_password(chef_node)
         return run_remote_ssh_cmd(ip, 'root', root_pass, '/etc/init.d/iptables save; /etc/init.d/iptables stop; /etc/init.d/iptables save')
 
+    def environment_has_controller(self, environment):
+        # Load Environment
+        nodes = Search('node').query("chef_environment:%s" % environment)
+        roles = ['role[qa-single-controller]', 'role[qa-ha-controller1]', 'role[qa-ha-controller2]']
+        for node in nodes:
+            chef_node = Node(node['name'])
+            if any(x in chef_node.run_list for x in roles):
+                return True
+            else:
+                return False
+
     def erase_node(self, chef_node):
         """
         @param chef_node
@@ -232,27 +243,36 @@ class rpcsqa_helper:
         self.razor.remove_active_model(am_uuid)
         time.sleep(15)
 
-    def razor_password(self, chef_node):
-        metadata = chef_node.attributes['razor_metadata'].to_dict()
-        uuid = metadata['razor_active_model_uuid']
-        return self.razor.get_active_model_pass(uuid)['password']
+    def gather_all_nodes(self, os):
+        # Gather the nodes for the requested OS
+        nodes = Search('node').query("name:qa-%s-pool*" % os)
+        return nodes
 
-    def remove_broker_fail(self, policy):
-        active_models = self.razor.simple_active_models(policy)
-        for active in active_models:
-            data = active_models[active]
-            if 'broker_fail' in data['current_state']:
-                print "!!## -- Removing active model  (broker_fail) -- ##!!"
-                root_pass = razor.get_active_model_pass(data['am_uuid'])['password']
-                ip = data['eth1_ip']
-                run = run_remote_ssh_cmd(ip, 'root', root_pass, 'reboot 0')
-                if run['success']:
-                    self.razor.remove_active_model(data['am_uuid'])
-                    time.sleep(15)
-                else:
-                    print "Trouble removing broker fail"
-                    print run
-                    sys.exit(1)
+    def gather_size_nodes(self, os, environment, cluster_size):
+        ret_nodes = []
+        count = 0
+
+        # Gather the nodes for the requested OS
+        nodes = Search('node').query("name:qa-%s-pool*" % os)
+
+        # Take a node from the default environment that has its network interfaces set.
+        for n in nodes:
+            name = n['name']
+            node = Node(name)
+            if ((node.chef_environment == "_default" or node.chef_environment == environment) and "recipe[network-interfaces]" in node.run_list):
+                self.set_nodes_environment(node, environment)
+                ret_nodes.append(name)          
+                print "Taking node: %s" % name
+                count += 1
+
+                if count >= cluster_size:
+                    break
+
+        if count < cluster_size:
+            print "Not enough available nodes for requested cluster size of %s, try again later..." % cluster_size
+            sys.exit(1)
+
+        return ret_nodes
 
     def install_opencenter(self, server, install_script, role, oc_server_ip='0.0.0.0'):
         chef_node = Node(server)
@@ -378,13 +398,35 @@ class rpcsqa_helper:
                 print "Exception: %s" % prepare_run['exception']
                 sys.exit(1)
 
-    def print_server_info(self, chef_node):
-        return "%s - %s" % (chef_node, chef_node['ipaddress'])
-
     def print_computes_info(self, computes):
         for compute in computes:
             c = Node(compute)
             print "Compute: %s" % self.print_server_info(c)
+
+    def print_server_info(self, chef_node):
+        return "%s - %s" % (chef_node, chef_node['ipaddress'])
+
+    def razor_password(self, chef_node):
+        metadata = chef_node.attributes['razor_metadata'].to_dict()
+        uuid = metadata['razor_active_model_uuid']
+        return self.razor.get_active_model_pass(uuid)['password']
+
+    def remove_broker_fail(self, policy):
+        active_models = self.razor.simple_active_models(policy)
+        for active in active_models:
+            data = active_models[active]
+            if 'broker_fail' in data['current_state']:
+                print "!!## -- Removing active model  (broker_fail) -- ##!!"
+                root_pass = razor.get_active_model_pass(data['am_uuid'])['password']
+                ip = data['eth1_ip']
+                run = run_remote_ssh_cmd(ip, 'root', root_pass, 'reboot 0')
+                if run['success']:
+                    self.razor.remove_active_model(data['am_uuid'])
+                    time.sleep(15)
+                else:
+                    print "Trouble removing broker fail"
+                    print run
+                    sys.exit(1)
 
     def run_chef_client(self, chef_node, logfile="STDOUT"):
         """
@@ -437,6 +479,15 @@ class rpcsqa_helper:
                 print "Final run: %s" % run3
                 raise Exception("Failed to set network interface for %s" % chef_node)
 
+    def set_node_in_use(self, node, role):
+        # Edit the controller in our chef
+        chef_node = Node(node)
+        chef_node['in_use'] = '%s' % role
+        node_ip = chef_node['ipaddress']
+        chef_node.save()
+
+        return node_ip
+
     def set_nodes_environment(self, chef_node, environment):
         print "Nodes environment is %s, trying to set to %s" % (chef_node.chef_environment, environment)
         if chef_node.chef_environment != environment:
@@ -454,54 +505,3 @@ class rpcsqa_helper:
         else:
             print "Platform Family %s is not supported." % chef_node['platform_family']
             sys.exit(1)
-
-    def gather_all_nodes(self, os):
-        # Gather the nodes for the requested OS
-        nodes = Search('node').query("name:qa-%s-pool*" % os)
-        return nodes
-
-    def gather_size_nodes(self, os, environment, cluster_size):
-        ret_nodes = []
-        count = 0
-
-        # Gather the nodes for the requested OS
-        nodes = Search('node').query("name:qa-%s-pool*" % os)
-
-        # Take a node from the default environment that has its network interfaces set.
-        for n in nodes:
-            name = n['name']
-            node = Node(name)
-            if ((node.chef_environment == "_default" or node.chef_environment == environment) and "recipe[network-interfaces]" in node.run_list):
-                self.set_nodes_environment(node, environment)
-                ret_nodes.append(name)          
-                print "Taking node: %s" % name
-                count += 1
-
-                if count >= cluster_size:
-                    break
-
-        if count < cluster_size:
-            print "Not enough available nodes for requested cluster size of %s, try again later..." % cluster_size
-            sys.exit(1)
-
-        return ret_nodes
-
-    def environment_has_controller(self, environment):
-        # Load Environment
-        nodes = Search('node').query("chef_environment:%s" % environment)
-        roles = ['role[qa-single-controller]', 'role[qa-ha-controller1]', 'role[qa-ha-controller2]']
-        for node in nodes:
-            chef_node = Node(node['name'])
-            if any(x in chef_node.run_list for x in roles):
-                return True
-            else:
-                return False
-
-    def set_node_in_use(self, node, role):
-        # Edit the controller in our chef
-        chef_node = Node(node)
-        chef_node['in_use'] = '%s' % role
-        node_ip = chef_node['ipaddress']
-        chef_node.save()
-
-        return node_ip
